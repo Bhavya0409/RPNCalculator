27a90c974b76ba9c4ccd386a845e0d5dc71d4297
Bhavya Shah <bhavyashah@Bhavyas-MBP.home>
2018-09-24 15:02:42 -0400

    added more features to use calculator

--- main.js
+++ main.js
@@ -2,68 +2,68 @@ const rl = require('readline');
 
 let i = rl.createInterface(process.stdin, process.stdout);
 
-i.question("Enter your mathematical infix expression. Supported characters include +-*/()\n", (input) => {
-  // 10.2*(8-6)/3+112.5
-  const stack = [];
-  const postfix = [];
-
-  let temp = '';
-
-  for (let i = 0; i < input.length; i++) {
-
-    if (!isNaN(input.charAt(i))) {
-      // If the character is a number, then keep checking numbers until an operator is reached.
-      temp += input.charAt(i);
-
-      while ((i+1) !== input.length && (!isNaN(input.charAt(i + 1)) || input.charAt(i+1) === '.')) {
-        i++;
-        temp += input.charAt(i);
-      }
-
-      // Outside of loop we have either end of string or operator. Either way, put temp into postfix and clear temp
-      postfix.push(temp);
-      temp = '';
+askQuestion = () => {
+  i.question("Enter your mathematical infix expression or \"quit\" to exit. Supported characters include +-*/()\n", (input) => {
+    if (input === 'quit') {
+      console.log('exiting...')
+      i.close();
+      process.stdin.destroy();
     } else {
-      // character is an operator
-      if (stack.length === 0 || input.charAt(i) === '(') {
-        stack.push(input.charAt(i));
-      } else {
-        if (input.charAt(i) === ')') {
-          while (stack[stack.length - 1] !== '(') {
-            postfix.push(stack.pop());
+      const stack = [];
+      const postfix = [];
+
+      let temp = '';
+
+      for (let i = 0; i < input.length; i++) {
+
+        if (!isNaN(input.charAt(i))) {
+          // If the character is a number, then keep checking numbers until an operator is reached.
+          temp += input.charAt(i);
+
+          while ((i+1) !== input.length && (!isNaN(input.charAt(i + 1)) || input.charAt(i+1) === '.')) {
+            i++;
+            temp += input.charAt(i);
           }
-          stack.pop();
+
+          // Outside of loop we have either end of string or operator. Either way, put temp into postfix and clear temp
+          postfix.push(temp);
+          temp = '';
         } else {
-          if (stack[stack.length - 1] === '(') {
+          // character is an operator
+          if (stack.length === 0 || input.charAt(i) === '(') {
             stack.push(input.charAt(i));
           } else {
-            while (stack.length !==  0 && stack[stack.length - 1] !== '(' && getPrecendence(input.charAt(i)) <= getPrecendence(stack[stack.length - 1])) {
-              postfix.push(stack.pop());
+            if (input.charAt(i) === ')') {
+              while (stack[stack.length - 1] !== '(') {
+                postfix.push(stack.pop());
+              }
+              stack.pop();
+            } else {
+              if (stack[stack.length - 1] === '(') {
+                stack.push(input.charAt(i));
+              } else {
+                while (stack.length !==  0 && stack[stack.length - 1] !== '(' && getPrecendence(input.charAt(i)) <= getPrecendence(stack[stack.length - 1])) {
+                  postfix.push(stack.pop());
+                }
+                stack.push(input.charAt(i));
+              }
             }
-            stack.push(input.charAt(i));
           }
         }
       }
+
+
+      while (stack.length !== 0) {
+        postfix.push(stack.pop());
+      }
+
+      printPostfix(postfix);
+
+      calculatePostfix(postfix);
+
+      askQuestion();
     }
-  }
-
-
-  while (stack.length !== 0) {
-    postfix.push(stack.pop());
-  }
-
-  printPostfix(postfix);
-
-  i.close();
-  process.stdin.destroy();
-});
-
-printPostfix = (postfix) => {
-  let str = ''
-  postfix.forEach((s) => {
-    str += s  + ' ';
-  })
-  console.log('postfix is:' + str);
+  });
 }
 
 getPrecendence = (char) => {
@@ -75,3 +75,46 @@ getPrecendence = (char) => {
     return 0;
   };
 };
+
+printPostfix = (postfix) => {
+  let str = ''
+  postfix.forEach((s) => {
+    str += s  + ' ';
+  })
+  console.log('postfix is:' + str);
+};
+
+calculatePostfix = (postfix) => {
+  const stack = [];
+  postfix.forEach((item) => {
+    if (isNaN(item)) {
+      const second = parseFloat(stack.pop());
+      const first = parseFloat(stack.pop());
+      switch (item) {
+        case '+':
+          stack.push(first + second);
+          break;
+        case '-':
+          stack.push(first - second);
+          break;
+        case '*':
+          stack.push(first * second);
+          break;
+        case '/':
+          stack.push(first / second);
+          break;
+          default:
+            stack.push(first);
+            stack.push(second);
+            break;
+      }
+    } else {
+      // item is a number
+      stack.push(item);
+    }
+  });
+
+  console.log(stack[0]);
+}
+
+askQuestion();
